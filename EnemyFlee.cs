727947485e679408e5450792e3ae35d906f5a166
demariandi12 <75235867+demariandi12@users.noreply.github.com>
2020-11-30 11:39:41 +0700

    source code AI

---
+++ EnemyFlee.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class EnemyFlee : MonoBehaviour 
+{
+    private NavMeshAgent _agent;
+    public GameObject Player;
+    public float EnemyDistanceRun = 4.0f;
+    void Start ()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+// Update is called once per frame
+    void Update () 
+    {
+        float distance = Vector3.Distance (transform.position, Player.transform.position);
+        
+        if (distance < EnemyDistanceRun)
+        {
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+            Vector3 newPos = transform.position + dirToPlayer;
+            _agent.SetDestination (newPos);
+        }
+    }
+}
---
+++ EnemyMovement.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.AI;
+public class EnemyMovement : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    public GameObject Player;
+    public float EnemyDistanceRun = 4.0f;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+    
+    void Start ()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+    }
+    void Awake ()
+    {
+        playerHealth = Player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent <EnemyHealth> ();
+    }
+    void Update ()
+    {
+        nav.SetDestination (Player.transform.position);
+        float distance = Vector3.Distance (transform.position, Player.transform.position);
+    
+        if (distance < EnemyDistanceRun)
+        {
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+            Vector3 newPos = transform.position + dirToPlayer;
+            _agent.SetDestination (newPos);
+        }
+    }
+}
